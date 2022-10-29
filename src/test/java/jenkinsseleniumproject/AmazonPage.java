ec1898b4d8f58ac4fe7d787fe678f7626a6c2d27
Raj <rajvendersingh50@gmail.com>
2022-10-29 09:28:03 +0530

    Test commit

---
+++ src/test/java/jenkinsseleniumproject/AmazonPage.java
@@ -0,0 +1,18 @@
+package jenkinsseleniumproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class AmazonPage {
+	
+	
+public static void main () {
+System.setProperty("webdriver.chrome.driver","H:\\Chromedriver\\chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+//get website 
+driver.get("https://www.amazon.com/");
+
+
+}
+}
