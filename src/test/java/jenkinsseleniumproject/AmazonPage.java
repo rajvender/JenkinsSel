2c48e25cbe80953910952201288d6dd4b486064e
Raj <rajvendersingh50@gmail.com>
2022-10-30 14:08:12 +0530

    Test

--- src/test/java/jenkinsseleniumproject/AmazonPage.java
+++ src/test/java/jenkinsseleniumproject/AmazonPage.java
@@ -11,7 +11,7 @@ public static void main (String args[]) {
 System.setProperty("webdriver.chrome.driver","H:\\Chromedriver\\chromedriver.exe");
 WebDriver driver=new ChromeDriver();
 driver.get("https://www.amazon.com/");
-driver.quit();
+driver.quit();;
 
 }
 }
